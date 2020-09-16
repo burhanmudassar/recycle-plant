4dc8d2627ad9d0051a55a2d0485982364e29c520
Burhan Mudassar <bmudassar3@gatech.edu>
2020-09-16 14:47:49 +0000

    First commit running with mobilenet v2

---
+++ data/createDataset.py
@@ -0,0 +1,85 @@
+import os
+from torch.utils.data import Dataset
+import numpy as np
+import cv2
+from torchvision.transforms import transforms
+from torchvision.transforms.transforms import RandomAffine, RandomPerspective, RandomRotation, RandomVerticalFlip, ToPILImage
+
+CLASSES = ("glass", "paper", "cardboard", "plastic", "metal", "trash")
+
+testTransform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(104, 117, 123), std=(57.4, 57.1, 58.4)),
+     
+])
+
+augmentTransform = transforms.Compose([
+        transforms.ToPILImage(),
+        # transforms.RandomCrop((300,300)),
+        transforms.RandomAffine(degrees=5, shear=10),
+        transforms.RandomRotation(degrees=5),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomPerspective(),
+])
+
+trainTransform = transforms.Compose([
+        *augmentTransform.transforms,
+        *testTransform.transforms
+        # transforms.Resize((224, 224)),
+        # transforms.ToTensor(),
+        # transforms.Normalize(mean=(104, 117, 123), std=(57.4, 57.1, 58.4)),
+])
+
+
+
+class TrashData(Dataset):
+
+    def __init__(self, root, set, transform=None):
+        self.root = root
+        self.path = os.path.join(self.root, 'dataset-resized')
+        self.nb_classes = len(CLASSES)
+        self.transform = transform
+        self.ids, self.targets = self.readsplitfile(set)
+
+        print(f"Count of classes in {set}")
+        for clsId, cls in enumerate(CLASSES):
+            print(f"{cls} : {np.sum(self.targets == clsId)}")
+
+    def __getitem__(self, index):
+        return self.getitem(index)
+
+    def __len__(self):
+        return len(self.ids)
+
+    def readsplitfile(self, set='train'):
+        filepaths = []
+        classes = []
+        with open(os.path.join(self.root, set+'list.txt'), 'r') as f:
+            for x in f.readlines():
+                line = x.split()
+                f_ = line[0]
+                c_ = int(line[1])
+                filepaths += [os.path.join(self.path, CLASSES[c_-1], f_)]
+                classes += [c_-1]
+
+        return filepaths, np.array(classes)
+
+    def getitem(self,index):
+        path = self.ids[index]
+        target = self.targets[index]
+
+        im = cv2.imread(path)
+        if im is None:
+            print("Invalid path")
+            raise ValueError
+
+        if self.transform:
+            im = self.transform(im)
+
+        return im, target
+
+
+
---
+++ main.py
@@ -0,0 +1,117 @@
+import os
+from data.createDataset import TrashData
+from data.createDataset import trainTransform
+from data.createDataset import testTransform
+import pytorch_lightning as pl
+from pytorch_lightning.metrics import ConfusionMatrix
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision.models import MobileNetV2
+from torchvision.models.resnet import resnet34
+from torchvision.models.mobilenet import mobilenet_v2
+import numpy as np
+import pandas as pd
+
+class Solver(pl.LightningModule):
+
+    def __init__(self, pretrained=False, num_classes=5):
+        super().__init__()
+        # Pretrained model
+        self.datapath = os.path.join(os.getcwd(), 'data')
+
+        if pretrained:
+            self.model = mobilenet_v2(pretrained=True)
+            for params in self.model.features.parameters(recurse=True):
+                params.requires_grad = False
+            output_features_nb = 1280
+            self.model.classifier = nn.Linear(output_features_nb, num_classes, bias=True)
+            torch.nn.init.xavier_normal_(self.model.classifier.weight, gain=1.0)
+            torch.nn.init.zeros_(self.model.classifier.bias)
+        # From scratch model
+        else:
+            features = MobileNetV2(num_classes=num_classes)
+            # Freeze the network except the classifier
+            for params in features.features.parameters(recurse=True):
+                params.requires_grad = False
+            self.model = features
+        
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.model(x)
+
+        if self.train:
+            return x
+        return self.softmax(x)
+
+    def training_step(self, batch, batch_nb):
+        x, y = batch
+        yhat = self(x)
+
+        loss = torch.nn.functional.cross_entropy(yhat, y)
+        tensorboard_logs = {'train_loss': loss}
+        return {'loss':loss, 'log':tensorboard_logs}
+
+    def validation_step(self, batch, batch_nb):
+        x, y = batch
+        yhat = self(x)
+        val_loss = torch.nn.functional.cross_entropy(yhat, y)
+        tp = (self.softmax(yhat).argmax(dim=1) == y).sum()
+        count = y.size(0)
+        return{'val_loss':val_loss, 'tp':tp, 'count':count}
+
+    def test_step(self, batch, batch_nb):
+        x, y = batch
+        yhat = self(x)
+        test_loss = torch.nn.functional.cross_entropy(yhat, y)
+
+        ypred = self.softmax(yhat).argmax(dim=1)
+        confusion_matrix = np.zeros((yhat.size(1),yhat.size(1)))
+
+        for y_, ypred_ in zip(y, ypred):
+            confusion_matrix[y_, ypred_] += 1
+
+        tp = (ypred == y).sum()
+        count = y.size(0)
+        return{'test_loss':test_loss, 'tp':tp, 'count':count, 'cmatrix':confusion_matrix}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        val_acc = torch.stack([x['tp'] for x in outputs]).sum() / float(sum([x['count'] for x in outputs])) * 100.0
+
+        logs = {'val_loss': avg_loss, 'val_acc':val_acc}
+        return {'val_loss':avg_loss, 'log':logs}
+
+    def test_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
+        test_acc = torch.stack([x['tp'] for x in outputs]).sum() / float(sum([x['count'] for x in outputs])) * 100.0
+        cmatrix = np.sum(np.stack([x['cmatrix'] for x in outputs], axis=0), axis=0)
+        
+        logs = {'test_loss': avg_loss, 'test_acc': test_acc}
+
+        return {'test_loss':avg_loss, 'log':logs, 'progress_bar':logs, 'cmatrix':cmatrix}
+
+    def train_dataloader(self) -> DataLoader:
+        trainSet = TrashData(self.datapath, set='train', transform=trainTransform)
+        return DataLoader(trainSet, batch_size=32, shuffle=True)
+    
+    def val_dataloader(self):
+        valSet = TrashData(self.datapath, set='val', transform=testTransform)
+        return DataLoader(valSet, batch_size=32)
+
+    def test_dataloader(self):
+        testSet = TrashData(self.datapath, set='val', transform=testTransform)
+        return DataLoader(testSet, batch_size=32)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.00005, weight_decay=0.0001)
+        # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=5e-7)
+        return [optimizer]
+
+if __name__ == '__main__':
+    solver = Solver(pretrained=True, num_classes=6)
+    trainer = pl.Trainer(gpus=1, max_epochs=5)
+    trainer.fit(solver)
+    trainer.test(solver, test_dataloaders=DataLoader(TrashData(solver.datapath, set='test', transform=testTransform), batch_size=32))
